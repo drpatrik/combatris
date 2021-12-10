9dcf8ea252fb77b558c084730a10c5ee5e7993eb
Patrik Tennberg <patrik67@gmail.com>
2021-12-10 16:51:11 +0100

    Fix bug where I was calling virtual function from constructor

--- combatris/src/game/panes/lines_sent.h
+++ combatris/src/game/panes/lines_sent.h
@@ -9,9 +9,14 @@ class LinesSent final : public TextPane, public EventListener {
 
   LinesSent(SDL_Renderer* renderer, const std::shared_ptr<Assets>& assets)
        : TextPane(renderer, kMatrixStartX - kMinoWidth - (kBoxWidth + kSpace),
-                  (kMatrixStartY - kMinoHeight) + kYOffs, "LINES SENT", assets) { Reset(); }
+                  (kMatrixStartY - kMinoHeight) + kYOffs, "LINES SENT", assets) {
+    SetCenteredText(std::to_string(0));
+  }
 
-  virtual void Reset() override { lines_sent_ = 0;  SetCenteredText(std::to_string(0)); }
+  virtual void Reset() override {
+    lines_sent_ = 0;
+    SetCenteredText(std::to_string(0));
+  }
 
   virtual void Update(const Event& event) override {
     if (!event.Is(Event::Type::BattleSendLines)) {
