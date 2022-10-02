8962eebc4da0c9f230f603524abe44238a575023
Patrik Tennberg <patrik67@gmail.com>
2022-10-02 22:39:02 +0200

    GetColor is now inline and returns colors from an std::array, not sure if better, but...

--- combatris/src/utility/color.h
+++ combatris/src/utility/color.h
@@ -2,10 +2,34 @@
 
 #include <SDL.h>
 
+#include <array>
+#include <cassert>
+
 namespace utility {
 
-enum class Color { None, Transparent, White, Blue, Red, Green, Black, Yellow, Cyan, Purple, Orange, Gray, SteelGray, Gold, Coral };
+enum Color { None, Transparent, White, Blue, Red, Green, Black, Yellow, Cyan, Purple, Orange, Gray, SteelGray, Gold, Coral, LastColor };
 
-SDL_Color GetColor(Color color, uint8_t alpha = 255);
+const std::array<SDL_Color, static_cast<size_t>(Color::LastColor)> kColors {{
+  { 0, 0, 0, 0 }, // None}
+  { 0, 0, 1, 0 }, // Transparent
+  { 255, 255, 255, 0 }, // White
+  { 0, 0, 240, 0 }, // Blue
+  { 240, 0, 0, 0 }, // Red
+  { 0, 240, 0, 0 }, // Green
+  { 0, 0, 0, 0 }, // Black
+  { 240, 240, 0, 0 }, // Yellow
+  { 0, 240, 240, 0 }, // Cyan
+  { 160, 0, 240, 0 }, // Purple
+  { 240, 160, 0, 0 }, // Orange
+  { 102, 102, 102, 0 }, // Gray
+  { 182, 195, 201, 0 }, // Steel Gray
+  { 255, 215, 0, 0 }, // Gold
+  { 255,127,80, 0 } // Coral
+  }};
+
+inline const SDL_Color GetColor(Color color, uint8_t alpha = 255) {
+  assert(color != Color::None && color != Color::LastColor);
+  return { kColors[color].r, kColors[color].g, kColors[color].b, alpha };
+}
 
 } // namespace utility
